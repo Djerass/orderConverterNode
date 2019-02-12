46aa168d4f13b853257386a006edc60d31fd987e
Djerass <dsaraula@yandex.ru>
2019-02-12 11:45:57 +0300

    added on/off button and started preparing UI and Back to changing URL

--- app.js
+++ app.js
@@ -6,21 +6,36 @@ const conv = require('./Converter');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const converter = new conv.Converter(__dirname+'/inputOutput');
+const state = {
+    folder: __dirname + '/inputOutput',
+    isRunning: false
+};
+const converter = new conv.Converter(state.folder);
+
+
 
 app.use(express.static(__dirname + '/wwwroot'));
 
-app.get('/start', (req, res) => {
-    converter.startTimer();
-    res.send({result:'start'});
+app.get('/onOff', (req, res) => {
+    if (state.isRunning) {
+        converter.stopTimer();
+        state.isRunning = false;
+        res.send(state);
+    } else {
+        converter.startTimer();
+        state.isRunning = true;
+        res.send(state);
+    }
 });
 
-app.get('/stop', (req, res) => {
-    converter.stopTimer();
-    res.send({result:'stop'});
+app.get('/getState', (req, res) => {
+    res.send(state);
 });
-
 
 app.listen(port, () => {
     console.log('Started op port 3000');
+});
+
+app.post('/updateUrl', function (req, res) {
+    res.send('POST request to homepage');
 });
--- wwwroot/script.js
+++ wwwroot/script.js
@@ -1,32 +1,50 @@
 const docObjects = {
-    btnStart : document.getElementById('btnStart'),
-    btnStop : document.getElementById('btnStop')
-}
+    bbtnStartStop: document.getElementById('btnStartStop'),
+    linkOnOff:  '/onOff',
+    linkGetState : '/getState',
+    inputForFolder : document.getElementById('inputForFolder'),
+    containerUpdateUrl : document.getElementById('containerUpdateUrl')
+};
+
+let state;
+
+const updateControlUI = () => {
+    if (state) {
+        docObjects.inputForFolder.value = state.folder;
+        if (state.isRunning){
+            docObjects.bbtnStartStop.innerText = 'Stop';
+            docObjects.inputForFolder.disabled = true;
+            docObjects.containerUpdateUrl.classList.add('update-btn');
+        } else {    
+            docObjects.bbtnStartStop.innerText = 'Start';
+            docObjects.inputForFolder.disabled = false;
+            docObjects.containerUpdateUrl.classList.remove('update-btn');
+        }
+    }    
+};
+const getState = () => {
+    fetch(docObjects.linkGetState).then((response) => {
+        return response.json();
+    }).then((json) =>{
+        state = json;
+        updateControlUI();
+    }).catch();
+};
 
 
 
-const init = ()=>{
-    btnStart.addEventListener('click',()=>{
-        fetch('/start').then((response)=>{
+
+
+const init = () => {
+    docObjects.bbtnStartStop.addEventListener('click', () => {
+        fetch(docObjects.linkOnOff).then((response) => {
             return response.json();
-        }).then((json)=>{
-            console.log(json.result);
+        }).then((json) => {
+           state = json;
+           updateControlUI();
         });
-
     });
-    btnStop.addEventListener('click',()=>{
-        fetch('/stop').then((response)=>{
-            return response.json();
-        }).then((json)=>{
-            console.log(json.result);
-        });
-
-    });
+    getState();   
 };
 
 init();
-
-
-
-
-
